2fabfb29e2c9e49eafc60b7237bea9dcd9808a07
danjos <danny_jtd@hotmail.com>
2019-10-03 09:30:20 +0300

    Add files via upload

--- cycle_detector.c
+++ cycle_detector.c
@@ -10,21 +10,21 @@ typedef struct     s_list
     struct s_list  *next;
 }                  t_list;
 
-int        cycle_detector(const t_list *list)
+int        cycle_detector(const t_list *list)//funcion que funciona con solo 15 lineas
 {
     const t_list	*slow;
     const t_list	*fast;
 
-    slow = list;
+    slow = list;//necesitamos tener dos para saber cuando se intersecan, si lo hacen, es un ciclo
     fast = list;
-    if (!list)
-        return (0);
-    while (fast && fast->next)
+    if (!list)// simple revision de que si existe o no
+        return (0);//si no esxiste entonces regresamos cero
+    while (fast && fast->next)//debe existir la lista y el puntero a la siguiente lista
     {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (fast == slow)
-            return (1);
+        slow = slow->next;//si slow era la primera lista, ahora es igual a la segunda lista
+        fast = fast->next->next;//fast ahora es igual a la tercera lista
+        if (fast == slow)//tenemos dos diferentes listas, y cuando sean iguales, tenemos un ciclo
+            return (1);// y regresamos 1
     }
     return (0);
 }
@@ -47,6 +47,7 @@ int main(void)
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
-    node4->next = 0;
+    node4->next = node1;
 
     printf("%d\n", cycle_detector(node1));
+}
